378bb5106288900109afc39aa8eadee93b056f15
javad-rzvn <95181554+javad-rzvn@users.noreply.github.com>
2023-05-16 07:41:57 +0000

    Deploying to gh-pages from  @ 6800b59cdad1705fa7b15446cf10917f7422a3bc 🚀

---
+++ _app/immutable/nodes/5.d8f1973b.js
@@ -0,0 +1,93 @@
+import{S as te,i as ae,s as re,k as i,a as D,y as W,q as z,J as oe,l as n,h as t,c as j,m as l,z as G,r as P,n as a,G as r,b as M,A as Q,H as ie,g as U,d as Z,B as ee,o as ne,O as se}from"../chunks/index.4e1ccaab.js";import{P as ce}from"../chunks/Page_header.6c34fdf2.js";import{P as le}from"../chunks/Post_meta.48fa27bf.js";const{document:q}=se;function de(V){let d,m,k,o,p,w,h,s,c,g,B,u,x,b,_,f,A,R,O,v,F,X,I,L,T;return p=new ce({}),s=new le({props:{category:"آب",date:"25 اردیبهشت 1402",reading_time:V[0]}}),{c(){d=i("meta"),m=i("meta"),k=D(),o=i("div"),W(p.$$.fragment),w=D(),h=i("div"),W(s.$$.fragment),c=D(),g=i("div"),B=D(),u=i("div"),x=i("div"),b=i("div"),_=i("div"),f=i("div"),A=z("A simple primary alert—check it out!"),R=D(),O=i("div"),v=i("div"),F=z("A simple warning alert—check it out!"),X=D(),I=i("div"),L=z(`
+
+  Add paragraph or remove elements to the DOM to change the result. Man bun
+  slow-carb raw denim, swag succulents raclette kickstarter palo santo YOLO
+  retro normcore microdosing biodiesel taiyaki. Kogi lumbersexual narwhal master
+  cleanse. Biodiesel pabst viral, letterpress leggings post-ironic polaroid
+  forage. Forage cred tacos palo santo biodiesel post-ironic vice XOXO heirloom
+  keffiyeh tbh live-edge stumptown letterpress banjo. Master cleanse banh mi
+  shabby chic street art neutra tbh enamel pin lumbersexual art party tote bag
+  narwhal 90's. Thundercats cray banh mi, post-ironic seitan aesthetic celiac
+  actually edison bulb austin hashtag intelligentsia taxidermy succulents.
+  Farm-to-table kickstarter cloud bread chartreuse cray tattooed, stumptown
+  chicharrones mustache YOLO wolf disrupt godard banjo. Beard 90's next level yr
+  four dollar toast 8-bit intelligentsia. Organic lomo poutine, direct trade
+  kogi fixie everyday carry normcore drinking vinegar kale chips retro schlitz
+  sustainable. Bespoke taxidermy hexagon, godard waistcoat gentrify listicle
+  normcore brunch venmo tacos photo booth cronut. Raclette irony gentrify,
+  shabby chic swag pitchfork chia offal cred taxidermy kombucha quinoa chambray.
+  Tattooed cloud bread DIY ramps everyday carry bushwick distillery echo park.
+  Slow-carb poutine cliche jean shorts put a bird on it literally pork belly.
+  Biodiesel pabst viral, letterpress leggings post-ironic polaroid forage.
+  Forage cred tacos palo santo biodiesel post-ironic vice XOXO heirloom keffiyeh
+  tbh live-edge stumptown letterpress banjo. Master cleanse banh mi shabby chic
+  street art neutra tbh enamel pin lumbersexual art party tote bag narwhal 90's.
+  Thundercats cray banh mi, post-ironic seitan aesthetic celiac actually edison
+  bulb austin hashtag intelligentsia taxidermy succulents. Farm-to-table
+  kickstarter cloud bread chartreuse cray tattooed, stumptown chicharrones
+  mustache YOLO wolf disrupt godard banjo. Beard 90's next level yr four dollar
+  toast 8-bit intelligentsia. Organic lomo poutine, direct trade kogi fixie
+  everyday carry normcore drinking vinegar kale chips retro schlitz sustainable.
+  Bespoke taxidermy hexagon, godard waistcoat gentrify listicle normcore brunch
+  venmo tacos photo booth cronut. Raclette irony gentrify, shabby chic swag
+  pitchfork chia offal cred taxidermy kombucha quinoa chambray. Tattooed cloud
+  bread DIY ramps everyday carry bushwick distillery echo park. Slow-carb
+  poutine cliche jean shorts put a bird on it literally pork belly. Bespoke
+  taxidermy hexagon, godard waistcoat gentrify listicle normcore brunch venmo
+  tacos photo booth cronut. Raclette irony gentrify, shabby chic swag pitchfork
+  chia offal cred taxidermy kombucha quinoa chambray. Tattooed cloud bread DIY
+  ramps everyday carry bushwick distillery echo park. Slow-carb poutine cliche
+  jean shorts put a bird on it literally pork belly. Bespoke taxidermy hexagon,
+  godard waistcoat gentrify listicle normcore brunch venmo tacos photo booth
+  cronut. Raclette irony gentrify, shabby chic swag pitchfork chia offal cred
+  taxidermy kombucha quinoa chambray. Tattooed cloud bread DIY ramps everyday
+  carry bushwick distillery echo park. Slow-carb poutine cliche jean shorts put
+  a bird on it literally pork belly.`),this.h()},l(e){const y=oe("svelte-4950ta",q.head);d=n(y,"META",{name:!0,content:!0}),m=n(y,"META",{name:!0,content:!0}),y.forEach(t),k=j(e),o=n(e,"DIV",{class:!0,id:!0});var E=l(o);G(p.$$.fragment,E),w=j(E),h=n(E,"DIV",{class:!0});var S=l(h);G(s.$$.fragment,S),c=j(S),g=n(S,"DIV",{class:!0}),l(g).forEach(t),S.forEach(t),E.forEach(t),B=j(e),u=n(e,"DIV",{class:!0});var $=l(u);x=n($,"DIV",{class:!0});var C=l(x);b=n(C,"DIV",{class:!0});var Y=l(b);_=n(Y,"DIV",{class:!0});var J=l(_);f=n(J,"DIV",{class:!0,role:!0});var H=l(f);A=P(H,"A simple primary alert—check it out!"),H.forEach(t),J.forEach(t),R=j(Y),O=n(Y,"DIV",{class:!0});var K=l(O);v=n(K,"DIV",{class:!0,role:!0});var N=l(v);F=P(N,"A simple warning alert—check it out!"),N.forEach(t),K.forEach(t),Y.forEach(t),C.forEach(t),X=j($),I=n($,"DIV",{class:!0}),l(I).forEach(t),L=P($,`
+
+  Add paragraph or remove elements to the DOM to change the result. Man bun
+  slow-carb raw denim, swag succulents raclette kickstarter palo santo YOLO
+  retro normcore microdosing biodiesel taiyaki. Kogi lumbersexual narwhal master
+  cleanse. Biodiesel pabst viral, letterpress leggings post-ironic polaroid
+  forage. Forage cred tacos palo santo biodiesel post-ironic vice XOXO heirloom
+  keffiyeh tbh live-edge stumptown letterpress banjo. Master cleanse banh mi
+  shabby chic street art neutra tbh enamel pin lumbersexual art party tote bag
+  narwhal 90's. Thundercats cray banh mi, post-ironic seitan aesthetic celiac
+  actually edison bulb austin hashtag intelligentsia taxidermy succulents.
+  Farm-to-table kickstarter cloud bread chartreuse cray tattooed, stumptown
+  chicharrones mustache YOLO wolf disrupt godard banjo. Beard 90's next level yr
+  four dollar toast 8-bit intelligentsia. Organic lomo poutine, direct trade
+  kogi fixie everyday carry normcore drinking vinegar kale chips retro schlitz
+  sustainable. Bespoke taxidermy hexagon, godard waistcoat gentrify listicle
+  normcore brunch venmo tacos photo booth cronut. Raclette irony gentrify,
+  shabby chic swag pitchfork chia offal cred taxidermy kombucha quinoa chambray.
+  Tattooed cloud bread DIY ramps everyday carry bushwick distillery echo park.
+  Slow-carb poutine cliche jean shorts put a bird on it literally pork belly.
+  Biodiesel pabst viral, letterpress leggings post-ironic polaroid forage.
+  Forage cred tacos palo santo biodiesel post-ironic vice XOXO heirloom keffiyeh
+  tbh live-edge stumptown letterpress banjo. Master cleanse banh mi shabby chic
+  street art neutra tbh enamel pin lumbersexual art party tote bag narwhal 90's.
+  Thundercats cray banh mi, post-ironic seitan aesthetic celiac actually edison
+  bulb austin hashtag intelligentsia taxidermy succulents. Farm-to-table
+  kickstarter cloud bread chartreuse cray tattooed, stumptown chicharrones
+  mustache YOLO wolf disrupt godard banjo. Beard 90's next level yr four dollar
+  toast 8-bit intelligentsia. Organic lomo poutine, direct trade kogi fixie
+  everyday carry normcore drinking vinegar kale chips retro schlitz sustainable.
+  Bespoke taxidermy hexagon, godard waistcoat gentrify listicle normcore brunch
+  venmo tacos photo booth cronut. Raclette irony gentrify, shabby chic swag
+  pitchfork chia offal cred taxidermy kombucha quinoa chambray. Tattooed cloud
+  bread DIY ramps everyday carry bushwick distillery echo park. Slow-carb
+  poutine cliche jean shorts put a bird on it literally pork belly. Bespoke
+  taxidermy hexagon, godard waistcoat gentrify listicle normcore brunch venmo
+  tacos photo booth cronut. Raclette irony gentrify, shabby chic swag pitchfork
+  chia offal cred taxidermy kombucha quinoa chambray. Tattooed cloud bread DIY
+  ramps everyday carry bushwick distillery echo park. Slow-carb poutine cliche
+  jean shorts put a bird on it literally pork belly. Bespoke taxidermy hexagon,
+  godard waistcoat gentrify listicle normcore brunch venmo tacos photo booth
+  cronut. Raclette irony gentrify, shabby chic swag pitchfork chia offal cred
+  taxidermy kombucha quinoa chambray. Tattooed cloud bread DIY ramps everyday
+  carry bushwick distillery echo park. Slow-carb poutine cliche jean shorts put
+  a bird on it literally pork belly.`),$.forEach(t),this.h()},h(){q.title="یک میلیون متر مکعب یعنی چقدر؟",a(d,"name","description"),a(d,"content",""),a(m,"name","keywords"),a(m,"content",""),a(g,"class","divider-top svelte-eesyw7"),a(h,"class","row justify-content-center align-items-center post-meta-container"),a(o,"class","container-fluid d-flex flex-column justify-content-between svelte-eesyw7"),a(o,"id","single-post"),a(f,"class","alert alert-primary"),a(f,"role","alert"),a(_,"class","col-6"),a(v,"class","alert alert-warning"),a(v,"role","alert"),a(O,"class","col-6"),a(b,"class","row"),a(x,"class","container"),a(I,"class","result"),a(u,"class","post-content svelte-eesyw7")},m(e,y){r(q.head,d),r(q.head,m),M(e,k,y),M(e,o,y),Q(p,o,null),r(o,w),r(o,h),Q(s,h,null),r(h,c),r(h,g),M(e,B,y),M(e,u,y),r(u,x),r(x,b),r(b,_),r(_,f),r(f,A),r(b,R),r(b,O),r(O,v),r(v,F),r(u,X),r(u,I),r(u,L),T=!0},p:ie,i(e){T||(U(p.$$.fragment,e),U(s.$$.fragment,e),T=!0)},o(e){Z(p.$$.fragment,e),Z(s.$$.fragment,e),T=!1},d(e){t(d),t(m),e&&t(k),e&&t(o),ee(p),ee(s),e&&t(B),e&&t(u)}}}function he(V){let d;return ne(()=>{var m=document.querySelector(".post-content").textContent.trim();function k(s){function c(g){return g.split(" ").length}return c(s)<=300?"کمتر از 1 دقیقه":"حدود "+Math.round(c(s)/200)+" دقیقه"}o=k(m);var o=document.querySelector(".result");const w=JSON.stringify(`
+    <div class="alert alert-warning" role="alert">
+          A simple warning alert—check it out!
+        </div>
+`,(s,c)=>typeof c=="string"&&c.startsWith("<")?c.replace(/</g,"&lt;"):c);console.log(w);var h=JSON.parse(w);o.innerHTML=h}),[d]}class be extends te{constructor(d){super(),ae(this,d,he,de,re,{})}}export{be as component};
