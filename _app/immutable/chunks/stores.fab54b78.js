83bf04c18eaafd3df52753055c38f4e7fd0ac9cf
javad-rzvn <95181554+javad-rzvn@users.noreply.github.com>
2023-05-16 16:17:47 +0000

    Deploying to gh-pages from  @ 0f9cf20297e97fddf62d52e9f1cd57c206dbcff6 🚀

---
+++ _app/immutable/chunks/stores.fab54b78.js
@@ -0,0 +1,1 @@
+import"./index.4e1ccaab.js";import{s as e}from"./singletons.088b3bc5.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
